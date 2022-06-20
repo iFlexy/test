4c1a7c71ef7b5b05ba344ab41ccdaca24f4e6be0
iFlexy <107810874+iFlexy@users.noreply.github.com>
2022-06-20 11:06:51 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/Options.js
+++ App/js/Options.js
@@ -68,6 +68,17 @@ xui.Class('App.Options', 'xui.Module',{
                 .setLabelCaption("Vessel Size")
             );
             
+            host.xui_ui_div19.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input41")
+                .setHoverPop("xui_ui_list22")
+                .setLeft("0.6em")
+                .setTop("6.6em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Sheath Size")
+            );
+            
             append(
                 xui.create("xui.UI.List")
                 .setHost(host,"xui_ui_list22")
@@ -97,6 +108,65 @@ xui.Class('App.Options', 'xui.Module',{
                 .setLeft("8.6em")
                 .setTop("3.933333333333333em")
                 .setVisibility("hidden")
+                .setValue("a")
+                .onItemSelected([
+                    {
+                        "desc" : "set value",
+                        "type" : "control",
+                        "target" : "xui_ui_input32",
+                        "args" : [
+                            "{page.xui_ui_input32.setUIValue()}",
+                            undefined,
+                            undefined,
+                            "{args[1] caption}"
+                        ],
+                        "method" : "setUIValue",
+                        "redirection" : "other:callback:call",
+                        "event" : 2
+                    },
+                    {
+                        "desc" : "hide self",
+                        "type" : "control",
+                        "target" : "xui_ui_list22",
+                        "args" : [ ],
+                        "method" : "hide"
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.List")
+                .setHost(host,"xui_ui_list43")
+                .setItems([
+                    {
+                        "id" : "a",
+                        "caption" : "5 Fr",
+                        "imageClass" : "xui-icon-number1"
+                    },
+                    {
+                        "id" : "b",
+                        "caption" : "6 Fr",
+                        "imageClass" : "xui-icon-number2"
+                    },
+                    {
+                        "id" : "c",
+                        "caption" : "7 Fr",
+                        "imageClass" : "xui-icon-number3"
+                    },
+                    {
+                        "id" : "d",
+                        "caption" : "8 Fr",
+                        "imageClass" : "xui-icon-number4"
+                    },
+                    {
+                        "id" : "e",
+                        "caption" : "> 8 Fr",
+                        "imageClass" : "xui-icon-number5"
+                    }
+                ])
+                .setLeft("8.6em")
+                .setTop("7.933333333333334em")
+                .setVisibility("visible")
                 .setValue("a")
                 .onItemSelected([
                     {
