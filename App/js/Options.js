0f0cb25af1a1870778f90e6f942951e7e830948b
iFlexy <107810874+iFlexy@users.noreply.github.com>
2022-06-21 13:27:59 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/Options.js
+++ App/js/Options.js
@@ -269,7 +269,7 @@ xui.Class('App.Options', 'xui.Module',{
                         "event" : 1,
                         "conditions" : [
                             {
-                                "left" : "{page.xui_ui_comboinput448}",
+                                "left" : "{page.xui_ui_comboinput476.getValue()}",
                                 "symbol" : "include",
                                 "right" : "Yes"
                             }
